4ad51bcd41ab7b8722cf070c73760ca8299d4279
yunheng luo <508126049@qq.com>
2026-06-01 18:05:57 +0800

    Fix Netlify function bundling

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,6 +1,12 @@
 import serverless from "serverless-http";
 import { createApp } from "../../apps/server/src/app.js";
 
-const app = await createApp();
+let cachedHandler: ReturnType<typeof serverless> | undefined;
 
-export const handler = serverless(app);
+export const handler = async (event: unknown, context: unknown) => {
+  if (!cachedHandler) {
+    const app = await createApp();
+    cachedHandler = serverless(app);
+  }
+  return cachedHandler(event, context);
+};
